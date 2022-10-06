a2ac61d71d7150618add021b5a06f792ea0dab95
unknown <cfmbido@gmail.com>
2022-10-06 19:29:06 +0700

    add feature students

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const PORT = 4000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+app.get("/students", (req, res) => {
+  res.send("Hello Students");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening to port: http://localhost:${PORT}`);
